9e607cb759dbf238c5edb1f955abf5ba234456a7
toccy <toccy@e8e8ed6c-164c-0410-afcf-9e9a7c7d8c10>
2006-09-05 13:51:48 +0000

    rewrited - events used as autovariables
    git-svn-id: https://core.forge.funambol.org/svn/core/top-level/trunk/3x/client-api/native@10101 e8e8ed6c-164c-0410-afcf-9e9a7c7d8c10

--- src/c++/common/event/FireEvent.cpp
+++ src/c++/common/event/FireEvent.cpp
@@ -1,201 +1,203 @@
-
-#include "event/FireEvent.h"
-#include "event/ManageListener.h"
-#include "event/constants.h"
+/*
+ * Copyright (C) 2003-2006 Funambol
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 #include "base/fscapi.h"
+#include "event/fireEvent.h"
+#include "event/ManageListener.h"
 
-bool fireSyncEvent(const char * msg, int type) {
 
-    ManageListener & manage = ManageListener::getInstance();
+//
+// Fire a SyncEvent
+//
+bool fireSyncEvent(const char* msg, int type) {
+
+    ManageListener& manage = ManageListener::getInstance();
+    if(manage.synclistener == NULL) {
+        return FALSE;
+    }
 
     unsigned long timestamp = (unsigned long)time(NULL);
-    SyncEvent* event = new SyncEvent(type, timestamp);
-
-    if(event) {
-        event->setMessage(msg);
-    }
-    else {
-        return FALSE;
+    // Create event (object alive in the scope of this function)
+    SyncEvent event(type, timestamp);
+    if(msg) {
+        event.setMessage(msg);
     }
 
-    if(manage.synclistener) {
-
-        switch(type) {
-          case SYNC_BEGIN:
-              manage.synclistener->syncBegin(*event);
-              break;
-          case SYNC_END:
-              manage.synclistener->syncEnd(*event);
-              break;
-          case SEND_INITIALIZATION:
-              manage.synclistener->sendInitialization(*event);
-              break;
-          case SEND_MODIFICATION:
-              manage.synclistener->sendModifications(*event);
-              break;
-          case SEND_FINALIZATION:
-              manage.synclistener->sendFinalization(*event);
-              break;
-          case SYNC_ERROR:
-              manage.synclistener->syncError(*event);
-              break;
-          default:
-              return FALSE;
-        }
-    }
-    else {
-        return FALSE;
+    switch(type) {
+      case SYNC_BEGIN:
+          manage.synclistener->syncBegin(event);
+          break;
+      case SYNC_END:
+          manage.synclistener->syncEnd(event);
+          break;
+      case SEND_INITIALIZATION:
+          manage.synclistener->sendInitialization(event);
+          break;
+      case SEND_MODIFICATION:
+          manage.synclistener->sendModifications(event);
+          break;
+      case SEND_FINALIZATION:
+          manage.synclistener->sendFinalization(event);
+          break;
+      case SYNC_ERROR:
+          manage.synclistener->syncError(event);
+          break;
+      default:
+          return FALSE;
     }
 
     return TRUE;
-
-    deleteFireEvent(event);
 }
 
-//Fire a Transport Event
+
+//
+// Fire a Transport Event
+//
 bool fireTransportEvent(unsigned long size, int type) {
 
-    ManageListener & manage = ManageListener::getInstance();
-
-    unsigned long timestamp = (unsigned long)time(NULL);
-    TransportEvent * event = new TransportEvent(size, type, timestamp);
-
-    if(manage.transportlistener) {
-
-        switch(type) {
-            case SEND_DATA_BEGIN:
-              manage.transportlistener->sendDataBegin(*event);
-              break;
-            case SEND_DATA_END:
-              manage.transportlistener->syncDataEnd(*event);
-              break;
-            case RECEIVE_DATA_BEGIN:
-              manage.transportlistener->receiveDataBegin(*event);
-              break;
-            case RECEIVE_DATA_END:
-              manage.transportlistener->receiveDataEnd(*event);
-              break;
-            case DATA_RECEIVED:
-              manage.transportlistener->receivingData(*event);
-              break;
-            default:
-              return FALSE;
-        }
-    }
-    else {
+    ManageListener& manage = ManageListener::getInstance();
+    if(manage.transportlistener == NULL) {
         return FALSE;
     }
 
-    return TRUE;
+    unsigned long timestamp = (unsigned long)time(NULL);
+    // Create event (object alive in the scope of this function)
+    TransportEvent event(size, type, timestamp);
 
-    deleteFireEvent(event);
+    switch(type) {
+        case SEND_DATA_BEGIN:
+          manage.transportlistener->sendDataBegin(event);
+          break;
+        case SEND_DATA_END:
+          manage.transportlistener->syncDataEnd(event);
+          break;
+        case RECEIVE_DATA_BEGIN:
+          manage.transportlistener->receiveDataBegin(event);
+          break;
+        case RECEIVE_DATA_END:
+          manage.transportlistener->receiveDataEnd(event);
+          break;
+        case DATA_RECEIVED:
+          manage.transportlistener->receivingData(event);
+          break;
+        default:
+          return FALSE;
+    }
+
+    return TRUE;
 }
 
+
+//
 // Fire a SyncSourceEvent
-bool fireSyncSourceEvent(const wchar_t* sourceURI, SyncMode mode, int type)
-{
-    ManageListener & manage = ManageListener::getInstance();
+//
+bool fireSyncSourceEvent(const char* sourceURI, SyncMode mode, int type) {
 
-    unsigned long timestamp = (unsigned long)time(NULL);
-    SyncSourceEvent * event = new SyncSourceEvent(sourceURI, mode,type, timestamp);
-
-    if(manage.syncsourcelistener) {
-
-        switch(type) {
-
-          case SYNC_SOURCE_BEGIN:
-              manage.syncsourcelistener->syncSourceBegin(*event);
-              break;
-          case SYNC_SOURCE_END:
-              manage.syncsourcelistener->syncSourceEnd(*event);
-              break;
-          default:
-              return FALSE;
-        }
-    }
-    else {
+    ManageListener& manage = ManageListener::getInstance();
+    if(manage.syncsourcelistener == NULL) {
         return FALSE;
     }
 
-    return TRUE;
+    unsigned long timestamp = (unsigned long)time(NULL);
+    // Create event (object alive in the scope of this function)
+    SyncSourceEvent event(sourceURI, mode,type, timestamp);
 
-    deleteFireEvent(event);
+    switch(type) {
+      case SYNC_SOURCE_BEGIN:
+          manage.syncsourcelistener->syncSourceBegin(event);
+          break;
+      case SYNC_SOURCE_END:
+          manage.syncsourcelistener->syncSourceEnd(event);
+          break;
+      default:
+          return FALSE;
+    }
+
+    return TRUE;
 }
 
+
+//
 // Fire a SyncItemEvent
-bool fireSyncItemEvent(const wchar_t* sourceURI,const wchar_t* itemKey, int type)
-{
-    ManageListener & manage = ManageListener::getInstance();
+//
+bool fireSyncItemEvent(const char* sourceURI,const wchar_t* itemKey, int type) {
 
-    unsigned long timestamp = (unsigned long)time(NULL);
-    SyncItemEvent * event = new SyncItemEvent(itemKey, sourceURI, type, timestamp);
-
-    if(manage.syncitemlistener) {
-
-        switch(type) {
-            case ITEM_ADDED_BY_SERVER:
-              manage.syncitemlistener->itemAddedByServer(*event);
-              break;
-            case ITEM_DELETED_BY_SERVER:
-              manage.syncitemlistener->itemDeletedByServer(*event);
-              break;
-            case ITEM_UPDATED_BY_SERVER:
-              manage.syncitemlistener->itemUpdatedByServer(*event);
-              break;
-            case ITEM_ADDED_BY_CLIENT:
-              manage.syncitemlistener->itemAddedByClient(*event);
-              break;
-            case ITEM_DELETED_BY_CLIENT:
-              manage.syncitemlistener->itemDeletedByClient(*event);
-              break;
-            case ITEM_UPDATED_BY_CLIENT:
-              manage.syncitemlistener->itemUpdatedByClient(*event);
-              break;
-            default:
-              return FALSE;
-        }
-    }
-    else {
+    ManageListener& manage = ManageListener::getInstance();
+    if(manage.syncitemlistener == NULL) {
         return FALSE;
     }
 
-    return TRUE;
+    unsigned long timestamp = (unsigned long)time(NULL);
+    // Create event (object alive in the scope of this function)
+    SyncItemEvent event(itemKey, sourceURI, type, timestamp);
 
-    deleteFireEvent(event);
+    switch(type) {
+        case ITEM_ADDED_BY_SERVER:
+          manage.syncitemlistener->itemAddedByServer(event);
+          break;
+        case ITEM_DELETED_BY_SERVER:
+          manage.syncitemlistener->itemDeletedByServer(event);
+          break;
+        case ITEM_UPDATED_BY_SERVER:
+          manage.syncitemlistener->itemUpdatedByServer(event);
+          break;
+        case ITEM_ADDED_BY_CLIENT:
+          manage.syncitemlistener->itemAddedByClient(event);
+          break;
+        case ITEM_DELETED_BY_CLIENT:
+          manage.syncitemlistener->itemDeletedByClient(event);
+          break;
+        case ITEM_UPDATED_BY_CLIENT:
+          manage.syncitemlistener->itemUpdatedByClient(event);
+          break;
+        default:
+          return FALSE;
+    }
+
+    return TRUE;
 }
 
+
+//
 // Fire a SyncStatusEvent
-bool fireSyncStatusEvent(const char* command, int statusCode, const wchar_t* uri, const wchar_t* itemKey, int type)
-{
-    ManageListener & manage = ManageListener::getInstance();
+//
+bool fireSyncStatusEvent(const char* command, int statusCode, const char* uri, const wchar_t* itemKey, int type) {
 
-    unsigned long timestamp = (unsigned long)time(NULL);
-    SyncStatusEvent * event = new SyncStatusEvent(statusCode, command, itemKey, uri, type, timestamp);
-
-    if(manage.syncstatuslistener) {
-        switch(type) {
-            case CLIENT_STATUS:
-                manage.syncstatuslistener->statusSending(*event);
-                break;
-            case SERVER_STATUS:
-                manage.syncstatuslistener->statusReceived(*event);
-                break;
-            default:
-                return FALSE;
-        }
-    }
-    else {
+    ManageListener& manage = ManageListener::getInstance();
+    if(manage.syncstatuslistener == NULL) {
         return FALSE;
     }
 
+    unsigned long timestamp = (unsigned long)time(NULL);
+    // Create event (object alive in the scope of this function)
+    SyncStatusEvent event(statusCode, command, itemKey, uri, type, timestamp);
+
+    switch(type) {
+        case CLIENT_STATUS:
+            manage.syncstatuslistener->statusSending(event);
+            break;
+        case SERVER_STATUS:
+            manage.syncstatuslistener->statusReceived(event);
+            break;
+        default:
+            return FALSE;
+    }
+
     return TRUE;
-
-    deleteFireEvent(event);
 }
 
-void deleteFireEvent(const BaseEvent* event) {
-
-    if(event)
-        delete event;
-}
--- src/include/common/event/FireEvent.h
+++ src/include/common/event/FireEvent.h
@@ -1,6 +1,23 @@
+/*
+ * Copyright (C) 2003-2006 Funambol
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
-#ifndef INCL_SET_LISTENER
-#define INCL_SET_LISTENER
+#ifndef INCL_FIRE_EVENTS
+#define INCL_FIRE_EVENTS
 
 #include "event/BaseEvent.h"
 #include "event/SyncEvent.h"
@@ -10,25 +27,65 @@
 #include "event/TransportEvent.h"
 #include "event/constants.h"
 
-/* A set of global functions to fire events from inside the Client API
-*/
+/* 
+ * A set of global functions to fire an event from inside the API.
+ */
 
-// Fire a SyncEvent
-bool fireSyncEvent(const char *msg, int type);
 
-// Fire a TransportEvent
+/*
+ * Fire a SyncEvent.
+ *
+ * @param msg  : the message of sync event
+ * @param type : the type of event to fire (see event/constants.h)
+ * @return     : TRUE if no errors, 
+ *               FALSE if syncListener not instantiated, or 'type' wrong
+ */
+bool fireSyncEvent(const char* msg, int type);
+
+/*
+ * Fire a TransportEvent.
+ *
+ * @param size : the data size
+ * @param type : the type of event to fire (see event/constants.h)
+ * @return     : TRUE if no errors, 
+ *               FALSE if transportListener not instantiated, or 'type' wrong
+ */
 bool fireTransportEvent(unsigned long size, int type);
 
-// Fire a SyncSourceEvent
-bool fireSyncSourceEvent(const wchar_t* sourceURI, SyncMode mode, int type);
+/*
+ * Fire a SyncSourceEvent.
+ *
+ * @param sourceURI : the source being synchronized
+ * @param mode      : type of the performed sync
+ * @param type      : the type of event to fire (see event/constants.h)
+ * @return          : TRUE if no errors, 
+ *                    FALSE if syncsourceListener not instantiated, or 'type' wrong
+ */
+bool fireSyncSourceEvent(const char* sourceURI, SyncMode mode, int type);
 
-// Fire a SyncItemEvent
-bool fireSyncItemEvent(const wchar_t* sourceURI, const wchar_t* itemKey, int type);
+/*
+ * Fire a SyncItemEvent.
+ *
+ * @param sourceURI : the source the item belongs to
+ * @param itemKey   : the item key (GUID from the server)
+ * @param type      : the type of event to fire (see event/constants.h)
+ * @return          : TRUE if no errors, 
+ *                    FALSE if syncitemListener not instantiated, or 'type' wrong
+ */
+bool fireSyncItemEvent(const char* sourceURI, const wchar_t* itemKey, int type);
 
-// Fire a SyncStatusEvent
-bool fireSyncStatusEvent(const char* command, int statusCode, const wchar_t* uri, const wchar_t* itemKey, int type);
+/*
+ * Fire a SyncStatusEvent.
+ *
+ * @param command   : the command the status relates to
+ * @param statusCode: the status code
+ * @param uri       : the source uri
+ * @param itemKey   : the key of the item this status relates to if it is in response of a modification command
+ * @param type      : the type of event to fire (see event/constants.h)
+ * @return          : TRUE if no errors, 
+ *                    FALSE if syncstatusListener not instantiated, or 'type' wrong
+ */
+bool fireSyncStatusEvent(const char* command, int statusCode, const char* uri, const wchar_t* itemKey, int type);
 
-// Delete Event object inside the firing methods
-void deleteFireEvent(const BaseEvent* event);
 
 #endif
