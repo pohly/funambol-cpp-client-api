9d40da48da7a9c572e5c7216edf9b3e7fd51fa67
toccy <toccy@e8e8ed6c-164c-0410-afcf-9e9a7c7d8c10>
2006-09-05 13:52:27 +0000

    deleted class SetListener - use global functions
    git-svn-id: https://core.forge.funambol.org/svn/core/top-level/trunk/3x/client-api/native@10102 e8e8ed6c-164c-0410-afcf-9e9a7c7d8c10

--- src/c++/common/event/SetListener.cpp
+++ src/c++/common/event/SetListener.cpp
@@ -1,100 +1,93 @@
+/*
+ * Copyright (C) 2003-2006 Funambol
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 
-#include "event/SetListener.h"
+#include "event/setListener.h"
 #include "event/ManageListener.h"
 
-bool SetListener::setSyncListener(SyncListener * listener) {
-		
-	ManageListener & manage = ManageListener::getInstance();
-	
-	manage.synclistener = listener;
-	
-	return TRUE;
+//
+// Set listeners:
+//
+void setSyncListener(SyncListener* listener) {
+    ManageListener& manage = ManageListener::getInstance();
+    manage.synclistener = listener;
 }
 
-bool SetListener::setTransportListener(TransportListener * listener) {
-                                                                                
-	ManageListener & manage = ManageListener::getInstance();
-
-        manage.transportlistener = listener;
-
-	return TRUE;
-}
-bool SetListener::setSyncSourceListener(SyncSourceListener * listener) {
-                                                                                
-	ManageListener & manage = ManageListener::getInstance();
-	
-        manage.syncsourcelistener = listener;
-
-	return TRUE;
-}
-bool SetListener::setSyncItemListener(SyncItemListener * listener) {
-                                                                                
-	ManageListener & manage = ManageListener::getInstance();	
-	
-        manage.syncitemlistener = listener;
-
-	return TRUE;
-}
-bool SetListener::setSyncStatusListener(SyncStatusListener * listener) {
-                                                                                
-	ManageListener & manage = ManageListener::getInstance();
-
-        manage.syncstatuslistener = listener;
-	
-	return TRUE;
+void setTransportListener(TransportListener* listener) {
+    ManageListener& manage = ManageListener::getInstance();
+    manage.transportlistener = listener;
 }
 
-bool SetListener::unsetSyncListener() {
-
-	ManageListener & manage = ManageListener::getInstance();	
-	
-	if(manage.synclistener) {
-		delete manage.synclistener;
-	}
-	
-	return TRUE;	
+void setSyncSourceListener(SyncSourceListener* listener) {
+    ManageListener& manage = ManageListener::getInstance();
+    manage.syncsourcelistener = listener;
 }
 
-bool SetListener::unsetTransportListener() {
-                                                                                
-        ManageListener & manage = ManageListener::getInstance();
-                                                                                
-        if(manage.transportlistener) {
-                delete manage.transportlistener;
-        }
-                                                                                
-        return TRUE;
-}
-bool SetListener::unsetSyncSourceListener() {
-                                                                                
-        ManageListener & manage = ManageListener::getInstance();
-                                                                                
-        if(manage.syncsourcelistener) {
-                delete manage.syncsourcelistener;
-        }
-                                                                                
-        return TRUE;
-}
-bool SetListener::unsetSyncItemListener() {
-                                                                                
-        ManageListener & manage = ManageListener::getInstance();
-                                                                                
-        if(manage.syncitemlistener) {
-                delete manage.syncitemlistener;
-        }
-                                                                                
-        return TRUE;
+void setSyncItemListener(SyncItemListener* listener) {
+    ManageListener& manage = ManageListener::getInstance();
+    manage.syncitemlistener = listener;
 }
 
-bool SetListener::unsetSyncStatusListener() {
-                                                                                                 
-        ManageListener & manage = ManageListener::getInstance();
-                                                                                                 
-        if(manage.syncstatuslistener) {
-                delete manage.syncstatuslistener;
-        }
-                                                                                                 
-        return TRUE;
+void setSyncStatusListener(SyncStatusListener* listener) {
+    ManageListener& manage = ManageListener::getInstance();
+    manage.syncstatuslistener = listener;
+}
+
+//
+// Unset listeners:
+//
+void unsetSyncListener() {
+    ManageListener& manage = ManageListener::getInstance();
+    if(manage.synclistener) {
+        delete manage.synclistener;
+        manage.synclistener = NULL;
+    }
+}
+
+void unsetTransportListener() {
+    ManageListener& manage = ManageListener::getInstance();
+    if(manage.transportlistener) {
+        delete manage.transportlistener;
+        manage.transportlistener = NULL;
+    }
+}
+
+void unsetSyncSourceListener() {
+    ManageListener& manage = ManageListener::getInstance();
+    if(manage.syncsourcelistener) {
+        delete manage.syncsourcelistener;
+        manage.syncsourcelistener = NULL;
+    }
+}
+
+void unsetSyncItemListener() {
+    ManageListener& manage = ManageListener::getInstance();
+    if(manage.syncitemlistener) {
+        delete manage.syncitemlistener;
+        manage.syncitemlistener = NULL;
+    }
+}
+
+void unsetSyncStatusListener() {
+    ManageListener& manage = ManageListener::getInstance();
+    if(manage.syncstatuslistener) {
+        delete manage.syncstatuslistener;
+        manage.syncstatuslistener = NULL;
+    }
 }
 
--- src/include/common/event/SetListener.h
+++ src/include/common/event/SetListener.h
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2003-2006 Funambol
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 #ifndef INCL_SET_LISTENER
 #define INCL_SET_LISTENER
@@ -8,43 +25,41 @@
 #include "event/SyncStatusListener.h"
 #include "event/TransportListener.h"
 
-/* SetListener class contains a set of methods to either set or unset Listeners
-	for various events
-*/
+/* 
+ * A set of global functions to either set or unset Listeners
+ * for various events.
+ */
 
-	class SetListener {
+// Set the SyncEvent Listener
+void setSyncListener(SyncListener* listener);
 
-	    public:
+// Set the TransportEvent Listener
+void setTransportListener(TransportListener* listener);
 
-		// Set the SyncEvent Listener
-		bool setSyncListener(SyncListener *listener);
+// Set the SyncSourceEvent Listener
+void setSyncSourceListener(SyncSourceListener* listener);
 
-		// Set the TransportEvent Listener
-                bool setTransportListener(TransportListener *listener);
+// Set the SyncItemEvent Listener
+void setSyncItemListener(SyncItemListener* listener);
 
-		// Set the SyncSourceEvent Listener
-                bool setSyncSourceListener(SyncSourceListener *listener);
+// Set the SyncStatusEvent Listener
+void setSyncStatusListener(SyncStatusListener* listener);
 
-		// Set the SyncItemEvent Listener
-                bool setSyncItemListener(SyncItemListener *listener);
 
-		// Set the SyncStatusEvent Listener
-                bool setSyncStatusListener(SyncStatusListener *listener);
+//removes the SyncEvent Listener
+void unsetSyncListener();
 
-		//removes the SyncEvent Listener
-                bool unsetSyncListener();
+// Removes the TransportEvent Listener
+void unsetTransportListener();
 
-		// Removes the TransportEvent Listener
-                bool unsetTransportListener();
+// Removes the SyncSourceEvent Listener
+void unsetSyncSourceListener();
 
-		// Removes the SyncSourceEvent Listener
-                bool unsetSyncSourceListener();
+// Removes the SyncItemEvent Listener
+void unsetSyncItemListener();
 
-		// Removes the SyncItemEvent Listener
-                bool unsetSyncItemListener();
+// Removes the SyncStatusEvent Listener
+void unsetSyncStatusListener();
 
-		// Removes the SyncStatusEvent Listener
-                bool unsetSyncStatusListener();	
-	};
 
 #endif
