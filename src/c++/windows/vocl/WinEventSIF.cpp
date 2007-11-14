a545ec0c0e343e79203c7ca05c59409e074bac62
toccy <toccy@e8e8ed6c-164c-0410-afcf-9e9a7c7d8c10>
2007-11-14 14:22:21 +0000

    WinEventSIF: class for managing event items for Windows Clients, SIF format
    git-svn-id: https://core.forge.funambol.org/svn/core/top-level/trunk/3x/client-api/native@20954 e8e8ed6c-164c-0410-afcf-9e9a7c7d8c10

---
+++ src/c++/windows/vocl/WinEventSIF.cpp
@@ -0,0 +1,269 @@
+ /*
+ * Copyright (C) 2007 Funambol, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY, TITLE, NONINFRINGEMENT or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307  USA
+*/
+
+#include "vocl/WinEventSIF.h"
+#include "vocl/VConverter.h"
+#include "vocl/constants.h"
+#include "base/stringUtils.h"
+#include "vocl/sifUtils.h"
+
+using namespace std;
+
+
+// Constructor
+WinEventSIF::WinEventSIF() {
+    sifFields = NULL;
+    sif = L"";
+}
+
+// Constructor: fills propertyMap parsing the passed SIF string
+WinEventSIF::WinEventSIF(const wstring dataString, const wchar_t** fields, const wchar_t** recFields) {
+    
+    sif = L"";
+    sifFields = fields;
+    recPatternSIF.setSifFields(recFields);
+
+    parse(dataString);
+}
+
+// Destructor
+WinEventSIF::~WinEventSIF() {
+}
+
+wstring& WinEventSIF::toString() {
+    
+    wstring propertyValue;
+    sif = L"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";   
+    sif += L"<appointment>\n";
+
+    sif += L"<SIFVersion>";
+    sif += SIF_VERSION;
+    sif += L"</SIFVersion>\n";
+
+    //
+    // Add all appointment properties
+    //
+    map<wstring,wstring>::iterator it = propertyMap.begin();       
+    while (it != propertyMap.end()) {        
+        propertyValue = it->second;      
+        propertyValue = adaptToSIFSpecs(it->first, propertyValue);
+        addPropertyToSIF(sif, it->first, propertyValue);
+        it ++;
+    }
+
+    //
+    // Append recurrence properties
+    //
+    wstring element;
+    if (getProperty(L"IsRecurring", element)) {
+        wstring recurrenceSIF = recPatternSIF.toString();
+        if(recurrenceSIF != L"") {
+            sif += recurrenceSIF;
+        }
+
+        //
+        // If recurring, append events exceptions
+        //
+        BOOL isRec = _wtoi(element.c_str());
+        if(isRec) {
+            list<wstring>::iterator it;
+
+            if (!excludeDate.size() && !includeDate.size()) {
+                sif += L"<Exceptions/>\n";
+            }
+            else {
+                sif += L"<Exceptions>\n";
+
+                // Exceptions: ExcludeDate
+                if (excludeDate.size() > 0) {
+                    for (it  = excludeDate.begin(); it != excludeDate.end(); it++) {
+                        wstring exDate = adaptToSIFSpecs(L"ExcludeDate", *it);
+                        addPropertyToSIF(sif, L"ExcludeDate", exDate);
+                    }
+                }
+                // Exceptions: IncludeDate (should be empty for Outlook and WM)
+                if (includeDate.size() > 0) {
+                    for (it  = includeDate.begin(); it != includeDate.end(); it++) {
+                        wstring rDate = adaptToSIFSpecs(L"IncludeDate", *it);
+                        addPropertyToSIF(sif, L"IncludeDate", rDate);
+                    }
+                }
+
+                sif += L"</Exceptions>\n";
+            }
+        }
+    }
+
+    //
+    // TODO: format recipients
+    //
+
+    sif += L"</appointment>";
+
+    return sif;
+}
+
+
+
+int WinEventSIF::parse(const wstring data) {
+    
+    if (!sifFields) {
+        LOG.error(ERR_SIFFIELDS_NULL);
+        return 1;
+    }
+    propertyMap.clear();
+    
+    // Check if <appointment> tag is present...
+    wstring::size_type pos = 0;
+    wstring itemTypeTag = L"<appointment>";    
+    pos = data.find(itemTypeTag, 0);
+    if (pos == wstring::npos) {
+        LOG.error("Tag '%ls' not found", itemTypeTag.c_str());
+        return 1;
+    }
+    wstring propertyValue;
+
+    //
+    // Set appointment properties
+    //
+    for (int i=0; sifFields[i]; i++) {
+        // Set only properties found!
+        if (!getElementContent(data, sifFields[i], propertyValue, 0)) {
+
+            replaceAll(L"&lt;",  L"<", propertyValue);
+            replaceAll(L"&gt;",  L">", propertyValue);
+            replaceAll(L"&amp;", L"&", propertyValue);
+            
+            propertyValue = adaptFromSIFSpecs(sifFields[i], propertyValue);
+            setProperty(sifFields[i], propertyValue);
+        }
+    } 
+
+    //
+    // Set recurrence properties
+    //
+    wstring element;
+    if (getProperty(L"IsRecurring", element)) {
+        BOOL isRec = _wtoi(element.c_str());
+        if(isRec) {
+
+            // Fill recPatternSIF propertyMap.
+            recPatternSIF.parse(data);
+
+            // Fill exceptions lists.
+            parseExceptions(data);
+        }
+    }
+
+    //
+    // TODO: parse recipients and fill recipients list
+    //
+
+    return 0;
+}
+
+
+WinRecurrenceSIF* WinEventSIF::getRecPattern() {
+    return &recPatternSIF;
+}
+
+
+
+wstring WinEventSIF::adaptToSIFSpecs(const wstring& propName, const wstring& propValue) {
+    
+    wstring propertyValue = L"";
+
+    if (propValue.length() == 8) {
+        if ( propName == L"Start"       || 
+             propName == L"ExcludeDate" ||
+             propName == L"IncludeDate" ) {
+            propertyValue = formatDateWithMinus(propValue);    
+        }
+        else if (propName == L"End") {
+            // the End value must be decremented of a day in allDayEvent appointment
+            DATE d;
+            stringTimeToDouble(propValue, &d);
+            d -= 1;
+            doubleToStringTime(propertyValue, d, true);
+            propertyValue = formatDateWithMinus(propertyValue);
+        }
+    }
+
+    if (propertyValue != L"") {
+        return propertyValue;
+    } 
+    return propValue;
+}
+
+
+
+wstring WinEventSIF::adaptFromSIFSpecs(const wstring& propName, const wstring& propValue) {
+
+    wstring propertyValue = L"";
+
+    // the End value must be incremented of a day in allDayEvent appointment
+    if ( propName == L"End" && 
+        (propValue.size() == 8 || propValue.size() == 10)) { // both format yyyyMMdd and yyyy-MM-dd
+        DATE d;
+        stringTimeToDouble(propValue, &d);
+        d += 1;
+        doubleToStringTime(propertyValue, d, true);                                
+    }
+
+
+    if (propertyValue != L"") {
+        return propertyValue;
+    } 
+    return propValue;
+}
+
+
+
+void WinEventSIF::parseExceptions(const wstring& sifString) {
+
+    wstring exString;
+
+    if (getElementContent(sifString, L"Exceptions", exString)) {
+        // Not found: nothing to do.
+        return;
+    }
+
+    // <Exceptions> not empty -> parse all exceptions
+    if (exString.size() > 0) {
+        wstring exDate, rDate;
+        wstring::size_type pos, start, end;
+
+        // Parse all <ExcludeDate>
+        pos = 0;
+        while ( !getElementContent(exString, L"ExcludeDate", exDate, pos, start, end) ) {
+            pos = end;
+            if (exDate.size() > 0) {
+                excludeDate.push_back(exDate);
+            }
+        }
+
+        // Parse all <IncludeDate>
+        pos = start = end = 0;
+        while ( !getElementContent(exString, L"IncludeDate", rDate, pos, start, end) ) {
+            pos = end;
+            if (rDate.size() > 0) {
+                includeDate.push_back(rDate);
+            }
+        }
+    }
+}
---
+++ src/include/windows/vocl/WineventSIF.h
@@ -0,0 +1,125 @@
+ /*
+ * Copyright (C) 2007 Funambol, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY, TITLE, NONINFRINGEMENT or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307  USA
+*/
+
+#ifndef INCL_WINEVENT_SIF
+#define INCL_WINEVENT_SIF
+
+/** @cond API */
+/** @addtogroup win_adapter */
+/** @{ */
+
+#include "vocl/VObject.h"
+#include "vocl/WinItem.h"
+#include "vocl/WinEvent.h"
+#include "vocl/WinRecurrenceSIF.h"
+
+using namespace std;
+
+
+/**
+ * Rapresents an event object for Windows Clients, for SIF format.
+ * The object can be filled passing a SIF string, or filling
+ * directly the map. Calling 'toString()' a SIF is formatted and returned.
+ */
+class WinEventSIF : public WinEvent {
+
+private:
+
+    /// Internal string formatted (SIF).
+    wstring sif;
+
+    /// NULL terminated array of SIF fields names.
+    const wchar_t** sifFields;
+
+    /// The recurrence pattern object for SIF data, containing recurring properties.
+    WinRecurrenceSIF recPatternSIF;
+
+    /// Parse appointment exceptions from SIF string, and fill exception lists.
+    void parseExceptions(const wstring& sifString);
+
+
+public:
+
+    /// Default Constructor
+    WinEventSIF();
+
+    /**
+     * Constructor: fills propertyMap parsing the passed SIF
+     * @param dataString  the input SIF string
+     * @param fields      the NULL terminated array of SIF fields
+     * @param fields      the NULL terminated array of SIF fields for recurrence pattern
+     */
+    WinEventSIF(const wstring dataString, const wchar_t** fields, const wchar_t** recFields);
+
+    /// Destructor
+    ~WinEventSIF();  
+
+
+    /// Returns a pointer to the (internally owned) WinRecurrenceSIF.
+    /// @note: overrides the method of WinEvent, to return a SIF formatted string.
+    WinRecurrenceSIF* getRecPattern();
+
+    
+     /**
+     * Parse a SIF string and fills the propertyMap.
+     * The map is cleared and will contain only found properties
+     * at the end of the parsing.
+     * @param dataString  input SIF string to be parsed
+     * @return            0 if no errors
+     */
+    int parse(const wstring dataString);
+   
+    /**
+     * Format and return a SIF string from the propertyMap.
+     * Not supported properties are ignored and so not formatted 
+     * as they don't have a correspondence in propertyMap.
+     * @return  the SIF string formatted, reference to internal wstring
+     */
+    wstring& toString();
+
+
+    /**
+    * Transform the value of the specified property according to the SIF specifications.
+    * The values are formatted following the vcard and icalendar specs that in some cases
+    * they are different from the SIF expectations. If there are no differences, propValue 
+    * is returned.
+    *
+    * @param  propName   [IN] the property name
+    * @param  propValue  [IN] the property value    
+    * @return            the converted value if necessary
+    */
+    wstring adaptToSIFSpecs(const wstring& propName, const wstring& propValue);
+
+
+    /**
+     * Transform the value of the specified property found in SIF structure to the 
+     * one expected by Outlook.
+     * The values are formatted following the vcard and icalendar specs that in some cases
+     * they are different from the SIF expectations. If there are no differences the same
+     * value is returned.
+     *
+     * @param  propName   the property name
+     * @param  propValue  the property value
+     * @return            the converted value if necessary
+     */
+    wstring adaptFromSIFSpecs(const wstring& propName, const wstring& propValue);
+    
+};
+/** @} */
+/** @endcond */
+#endif
