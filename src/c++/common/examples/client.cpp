f69a8ca77b2caa131fe2761ad2b43571068f46f6
mvitolo <mvitolo@e8e8ed6c-164c-0410-afcf-9e9a7c7d8c10>
2008-01-03 14:59:23 +0000

    removed embedded tests in client.cpp
    all the tests in the old client.cpp are moved in properly class and corrected to work properly with the cppunit framework
    
    
    git-svn-id: https://core.forge.funambol.org/svn/core/top-level/trunk/3x/client-api/native@22035 e8e8ed6c-164c-0410-afcf-9e9a7c7d8c10

--- src/c++/common/examples/client.cpp
+++ src/c++/common/examples/client.cpp
@@ -34,10 +34,10 @@
  */
 
 #include "base/fscapi.h"
-// Define the test configuration
-#include "examples/config.h"
 
-//#include "crtdbg.h"
+#if 0
+#include "crtdbg.h"
+#endif
 #include "base/messages.h"
 #include "base/Log.h"
 #include "base/util/ArrayList.h"
@@ -46,44 +46,19 @@
 #include "spds/constants.h"
 #include "client/SyncClient.h"
 #include "client/DMTClientConfig.h"
-#ifdef TEST_SYNCSOURCE
+
 #include "examples/TestSyncSource.h"
 #include "examples/TestSyncSource2.h"
-#endif
-#include "filter/AllClause.h"
-#include "filter/ClauseUtil.h"
-#include "filter/LogicalClause.h"
-#include "filter/FieldClause.h"
-#include "filter/SourceFilter.h"
-#include "filter/WhereClause.h"
-#include "syncml/core/core.h"
-#include "syncml/formatter/Formatter.h"
+
 #include "spds/DefaultConfigFactory.h"
 
-#ifdef TEST_EVENT_HANDLING
 #include "examples/listeners/TestSyncListener.h"
 #include "examples/listeners/TestSyncSourceListener.h"
 #include "examples/listeners/TestSyncStatusListener.h"
 #include "examples/listeners/TestSyncItemListener.h"
 #include "examples/listeners/TestTransportListener.h"
 #include "event/SetListener.h"
-#endif
 
-#ifdef TEST_FILTER
-void testFilter();
-#endif
-#ifdef TEST_CLAUSE
-void testClause();
-#endif
-#ifdef TEST_CONFIG_FILTER
-void testConfigFilter();
-#endif
-#ifdef TEST_ENCRYPTION
-void testEncryption();
-#endif
-#ifdef TEXT_XMLPROCESSOR
-static void testXMLProcessor();
-#endif
 
 void createConfig(DMTClientConfig& config);
 
@@ -123,18 +98,8 @@ int main(int argc, char** argv) {
     _CrtSetDbgFlag( tmpFlag );
 #endif
 
-#ifdef TEST_ENCODE
-    WCHAR *content = loadAndConvert(TEXT("message.xml"), TEXT("base64"));
-    if(!content) {
-        fprintf(stderr, "Error in uudecode.");
-        exit(1);
-    }
-    convertAndSave(TEXT("message_out.xml"), content, TEXT("base64"));
-#endif
 
-
-#ifdef TEST_EVENT_HANDLING
-
+// ------------- Main sample client ------------------------
     //
     // Set listeners:
     //
@@ -149,15 +114,6 @@ int main(int argc, char** argv) {
     setSyncStatusListener(listener3);
     setSyncItemListener  (listener4);
     setTransportListener (listener5);
-
-    #ifndef TEST_SYNCSOURCE
-    #define TEST_SYNCSOURCE  1
-    #endif
-
-#endif
-
-// ------------- Main sample client ------------------------
-#ifdef TEST_SYNCSOURCE
 
     //
     // Create the configuration.
@@ -195,11 +151,7 @@ int main(int argc, char** argv) {
     // Save config to registry.
     config.save();
 
-#endif
 // ----------------------------------------------------------
-
-
-#ifdef TEST_EVENT_HANDLING
 
     //
     // Unset Listeners
@@ -210,117 +162,9 @@ int main(int argc, char** argv) {
     unsetSyncItemListener  ();
     unsetTransportListener ();
 
-#endif
-
-
-#ifdef TEST_SYNC_ENCRYPTION
-	Sync4jClient& s4j = Sync4jClient::getInstance();
-    s4j.setDMConfig(APPLICATION_URI);
-
-    TestSyncSource source = TestSyncSource(TEXT("briefcase"));
-
-    SyncSource** ssArray = new SyncSource*[2];
-    ssArray[0] = &source;
-    ssArray[1] = NULL;
-    s4j.sync(ssArray);
-#endif
-
-#ifdef TEST_ENCRYPTION
-	testEncryption();
-#endif
-
-#ifdef TEST_FILTER
-    testFilter();
-#endif
-
-#ifdef TEST_CLAUSE
-    testClause();
-#endif
-
-#ifdef TEST_CONFIG_FILTER
-    testConfigFilter();
-#endif
-
-
-
-#ifdef TEST_XMLPROCESSOR
-    testXMLProcessor();
-#endif
     return 0;
 }
 
-#ifdef TEST_XMLPROCESSOR
-static void testXMLProcessor(void)
-{
-    const char xml1[] =
-        "<document>\n\
-            <LocURI>./devinf11</LocURI>\n\
-            <plaintag>\n\
-                <attrtag attr=\"val\">content</attrtag>\n\
-            </plaintag>\n\
-            <emptytag/>\n\
-         </document>" ;
-
-    unsigned int pos = 0, start = 0, end = 0;
-    const char *p = 0;
-
-    // Get 'document' tag
-    char *doc = XMLProcessor::copyElementContent(xml1, "document", &pos);
-    LOG.debug("Document: '%s'", doc);
-    LOG.debug("xml[pos]= '%s'", xml1 + pos);
-
-    char buf[256];
-
-    // Get 'plaintag' content, using start/end pos
-    if(!XMLProcessor::getElementContent(doc, "plaintag", &pos, &start, &end)){
-        LOG.error("TEST FAILED.");
-        return;
-    }
-    memset(buf, 0, 255);
-    memcpy(buf, doc+start, end-start);
-    LOG.debug("Plaintag: '%s'", buf);
-
-    // Get 'LocURI' content, using start/end pos
-    if(!XMLProcessor::getElementContent(doc, "LocURI", &pos, &start, &end)){
-        LOG.error("TEST FAILED.");
-        return;
-    }
-    memset(buf, 0, 255);
-    memcpy(buf, doc+start, end-start);
-    LOG.debug("LocURI: '%s'", buf);
-
-    // Get 'attrtag' content, using start/end pos
-    if(!XMLProcessor::getElementContent(doc, "attrtag", &pos, &start, &end)){
-        LOG.error("TEST FAILED.");
-        return;
-    }
-    memset(buf, 0, 255);
-    memcpy(buf, doc+start, end-start);
-    LOG.debug("Attrtag: '%s'", buf);
-
-    // Get 'attrtag' attr list, using start/end pos
-    if(!XMLProcessor::getElementAttributes(doc, "attrtag", &start, &end)){
-        LOG.error("TEST FAILED.");
-        return;
-    }
-    memset(buf, 0, 255);
-    memcpy(buf, doc+start, end-start);
-    LOG.debug("Attrlist: '%s'", buf);
-
-    // Get 'emptytag' content, that should be empty
-    const char*empty = XMLProcessor::copyElementContent(doc, "emptytag");
-    if(!empty){
-        LOG.error("TEST FAILED.");
-        return;
-    }
-    LOG.debug("Emptytag: '%s'", empty);
-
-    if(doc)
-        delete [] doc;
-    if (empty)
-        delete [] empty;
-}
-#endif
 
 //
 // Function to create a default config.
--- test/common/event/testEvent.cpp
+++ test/common/event/testEvent.cpp
@@ -33,25 +33,14 @@
  * the words "Powered by Funambol".
  */
 
-#ifndef INCL_CONFIG_H
-#define INCL_CONFIG_H
-/** @cond DEV */
-
-// Use these defines to activate the desired test code
-
-// Test a simple sync with one sync source
-#define TEST_SYNCSOURCE      1
-
-// Test the encryption routines
-//#define TEST_SYNC_ENCRYPTION 1
-
-// Test the character encoding conversion
-//#define TEST_ENCODE          1
-
-
-// Test the Event Handling Framework
-//#define TEST_EVENT_HANDLING 1
-
-
-/** @endcond */
-#endif // INCL_CONFIG_H
+//
+//# include <cppunit/extensions/TestFactoryRegistry.h>
+//# include <cppunit/extensions/HelperMacros.h>
+//
+//class testEvent : public CppUnit::TestFixture {
+//
+//
+//    CPPUNIT_TEST_SUITE(testEvent);
+//    CPPUNIT_TEST_SUITE_END();
+//
+//}
