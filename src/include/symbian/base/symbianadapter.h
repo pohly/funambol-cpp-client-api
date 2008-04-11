a0192719ab16e4ec34decb22e4206c3cb30b9fb2
marco_garatti <marco_garatti@e8e8ed6c-164c-0410-afcf-9e9a7c7d8c10>
2008-04-11 12:04:40 +0000

    Fixed NAMESPACE problem and added missing license.
    git-svn-id: https://core.forge.funambol.org/svn/core/top-level/trunk/3x/client-api/native@24041 e8e8ed6c-164c-0410-afcf-9e9a7c7d8c10

--- src/include/symbian/base/symbianadapter.h
+++ src/include/symbian/base/symbianadapter.h
@@ -1,5 +1,42 @@
+/*
+ * Funambol is a mobile platform developed by Funambol, Inc. 
+ * Copyright (C) 2003 - 2007 Funambol, Inc.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by
+ * the Free Software Foundation with the addition of the following permission 
+ * added to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED
+ * WORK IN WHICH THE COPYRIGHT IS OWNED BY FUNAMBOL, FUNAMBOL DISCLAIMS THE 
+ * WARRANTY OF NON INFRINGEMENT  OF THIRD PARTY RIGHTS.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License 
+ * along with this program; if not, see http://www.gnu.org/licenses or write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA.
+ * 
+ * You can contact Funambol, Inc. headquarters at 643 Bair Island Road, Suite 
+ * 305, Redwood City, CA 94063, USA, or at email address info@funambol.com.
+ * 
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ * 
+ * In accordance with Section 7(b) of the GNU Affero General Public License
+ * version 3, these Appropriate Legal Notices must retain the display of the
+ * "Powered by Funambol" logo. If the display of the logo is not reasonably 
+ * feasible for technical reasons, the Appropriate Legal Notices must display
+ * the words "Powered by Funambol".
+ */
+
 #ifndef SYMBIANADAPTER_H_
 #define SYMBIANADAPTER_H_
+
+#include "base/globalsdef.h"
 
 #include <e32def.h>
 #include <stdio.h>
@@ -29,6 +66,7 @@
 #define USE_WCHAR   1
 #define TEXT(_x)    L##_x
 
+BEGIN_NAMESPACE
 
 /* These functions miss in the Symbian libc and are implemented here */
 wchar_t * wcsncpy (wchar_t *dst, const wchar_t *src, size_t count);
@@ -42,6 +80,8 @@ WCHAR *wcstok(WCHAR *ws1, const WCHAR *ws2);
 WCHAR *wcsncat(WCHAR *ws1, const WCHAR *ws2, size_t n);
 double wcstod(const WCHAR *nptr, WCHAR ** endptr);
 int _wtoi(const WCHAR *str);
+
+END_NAMESPACE
 
 /* Symbian does not ship a stdint.h interface (unless you use PIPS or OpenC)
  * We define the types used in the library, but at the moment we are not
