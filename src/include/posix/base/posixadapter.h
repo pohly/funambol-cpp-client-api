f378929ddc6e2df77942fd23a7a96f9b92ea1ef8
gazza <gazza@e8e8ed6c-164c-0410-afcf-9e9a7c7d8c10>
2008-06-26 17:55:50 +0000

    Posix adapter separated into the public part and a private part included only to build the API, under ifdef FUNAMBOL_BUILD_API
    git-svn-id: https://core.forge.funambol.org/svn/core/top-level/trunk/3x/client-api/native@25903 e8e8ed6c-164c-0410-afcf-9e9a7c7d8c10

--- src/include/posix/base/posixadapter.h
+++ src/include/posix/base/posixadapter.h
@@ -67,67 +67,15 @@
 # define __cdecl
 #endif
 
-// Enable the fix for encoding when building for posix
-#define VOCL_ENCODING_FIX
-
-
 #ifdef USE_WCHAR
 
-#undef WCHAR
-#include <wchar.h>
-#include <wctype.h>
+# undef WCHAR
+# include <wchar.h>
+# include <wctype.h>
 
-#define WCHAR wchar_t
-#define WCHAR_PRINTF "s"
-#define TEXT(_x) L##_x
-
-#include "base/globalsdef.h"
-
-BEGIN_NAMESPACE
-
-// FIXME: remove this and adapt VOCL.
-WCHAR *wcstok(WCHAR *s, const WCHAR *delim);
-
-inline int _wtoi(const WCHAR *s) { return (int)wcstol(s, NULL, 10); }
-
-END_NAMESPACE
-
-#define _wcsicmp wcscasecmp
-#define wcsicmp wcscasecmp
-#define snwprintf swprintf
-
-#ifdef MAC
-
-#   undef  _wcsicmp
-#   undef  wcsicmp
-
-inline int wcsicmp(const WCHAR * s1, const WCHAR * s2)
-{
-    const WCHAR * i1 = s1;
-    const WCHAR * i2 = s2;
-    
-    while (i1 != NULL && i2 != NULL)
-    {
-        if (*i1 > *i2)
-            return 1;
-        if (*i1 < *i2)
-            return -1;
-            
-        i1++;
-        i2++;
-    }
-    if (i1 == NULL && i2 == NULL)
-        return 0;
-    if (i1 == NULL)
-        return -1;
-    if (i2 == NULL)
-        return 1;
-    // Should never happen
-    return 0;
-}
-
-#   define _wcsicmp wcsicmp
-#endif
+# define WCHAR wchar_t
+# define WCHAR_PRINTF "ls"
+# define TEXT(_x) L##_x
 
 #else
 
@@ -137,46 +85,16 @@ inline int wcsicmp(const WCHAR * s1, const WCHAR * s2)
 #       define WCHAR_PRINTF "s"
 #       define TEXT(_x) _x
 
-#       define SYNC4J_LINEBREAK "\n"
+#endif
 
-#       define wsprintf sprintf
-#       define _wfopen fopen
-#       define wprintf printf
-#       define fwprintf fprintf
-#       define wsprintf sprintf
-#       define swprintf snprintf
-#       define snwprintf snprintf
-#       define wcscpy strcpy
-#       define wcsncpy strncpy
-#       define wcsncmp strncmp
-#       define wcslen strlen
-#       define wcstol strtol
-#       define wcstoul strtoul
-#       define wcsstr strstr
-#       define wcscmp strcmp
-#       define wcstok strtok
-        inline char towlower(char x) { return tolower(x); }
-        inline char towupper(char x) { return toupper(x); }
-#       define wmemmove memmove
-#       define wmemcpy memcpy
-#       define wmemcmp memcmp
-#       define wmemset memset
-#       define wcschr strchr
-#       define wcsrchr strrchr
-#       define wcscat strcat
-#       define wcsncat strncat
-#       define _wtoi atoi
-#       define wcstod strtod
-#       define wcsicmp strcasecmp
-#       define _wcsicmp strcasecmp
-#       define _stricmp strcasecmp
-
+#ifdef FUNAMBOL_BUILD_API
+# include "base/posix_build_adapter.h"
 #endif
 
 /* some of the code compares NULL against integers, which
    fails if NULL is defined as (void *)0 */
-#undef NULL
-#define NULL 0
+//#undef NULL
+//#define NULL 0
 
 #define min(x,y) ( (x) < (y) ? (x) : (y) )
 #define max(x,y) ( (x) > (y) ? (x) : (y) )
