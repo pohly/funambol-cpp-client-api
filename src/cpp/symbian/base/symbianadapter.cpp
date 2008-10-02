fb83deef7574042622b1c14e36b851e93f90b926
toccy <toccy@e8e8ed6c-164c-0410-afcf-9e9a7c7d8c10>
2008-10-02 11:04:45 +0000

    Added required functions
    git-svn-id: https://core.forge.funambol.org/svn/core/tags/r_symbian_7_1_5/funambol/client-api/native@29092 e8e8ed6c-164c-0410-afcf-9e9a7c7d8c10

--- src/cpp/symbian/base/symbianadapter.cpp
+++ src/cpp/symbian/base/symbianadapter.cpp
@@ -456,5 +456,19 @@ StringBuffer getCacheDirectory() {
     return empty;
 }
 
+// TODO: implementation needed!
+bool removeFileInDir(const char* dir, const char* filename) {
+    return true;
+}
+
+StringBuffer getHomeFolder() {
+    StringBuffer home(getenv("HOME"));
+    if( home.null() ) {
+        LOG.error("Home directory not defined.");
+    }
+    return home;
+}
+
+
 END_NAMESPACE
 
