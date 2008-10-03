19b715d9a447ec2a05a93bdd8a6d77fffa89fcc7
gazza <gazza@e8e8ed6c-164c-0410-afcf-9e9a7c7d8c10>
2008-10-03 04:50:18 +0000

    Added load test.
    git-svn-id: https://core.forge.funambol.org/svn/core/trunk/funambol/client-api/native@29110 e8e8ed6c-164c-0410-afcf-9e9a7c7d8c10

--- test/common/base/util/ArrayListTest.cpp
+++ test/common/base/util/ArrayListTest.cpp
@@ -1,37 +1,37 @@
 /*
- * Funambol is a mobile platform developed by Funambol, Inc. 
- * Copyright (C) 2003 - 2007 Funambol, Inc.
- * 
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Affero General Public License version 3 as published by
- * the Free Software Foundation with the addition of the following permission 
- * added to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED
- * WORK IN WHICH THE COPYRIGHT IS OWNED BY FUNAMBOL, FUNAMBOL DISCLAIMS THE 
- * WARRANTY OF NON INFRINGEMENT  OF THIRD PARTY RIGHTS.
- * 
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- * details.
- * 
- * You should have received a copy of the GNU Affero General Public License 
- * along with this program; if not, see http://www.gnu.org/licenses or write to
- * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301 USA.
- * 
- * You can contact Funambol, Inc. headquarters at 643 Bair Island Road, Suite 
- * 305, Redwood City, CA 94063, USA, or at email address info@funambol.com.
- * 
- * The interactive user interfaces in modified source and object code versions
- * of this program must display Appropriate Legal Notices, as required under
- * Section 5 of the GNU Affero General Public License version 3.
- * 
- * In accordance with Section 7(b) of the GNU Affero General Public License
- * version 3, these Appropriate Legal Notices must retain the display of the
- * "Powered by Funambol" logo. If the display of the logo is not reasonably 
- * feasible for technical reasons, the Appropriate Legal Notices must display
- * the words "Powered by Funambol".
- */
+* Funambol is a mobile platform developed by Funambol, Inc. 
+* Copyright (C) 2003 - 2007 Funambol, Inc.
+* 
+* This program is free software; you can redistribute it and/or modify it under
+* the terms of the GNU Affero General Public License version 3 as published by
+* the Free Software Foundation with the addition of the following permission 
+* added to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED
+* WORK IN WHICH THE COPYRIGHT IS OWNED BY FUNAMBOL, FUNAMBOL DISCLAIMS THE 
+* WARRANTY OF NON INFRINGEMENT  OF THIRD PARTY RIGHTS.
+* 
+* This program is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+* details.
+* 
+* You should have received a copy of the GNU Affero General Public License 
+* along with this program; if not, see http://www.gnu.org/licenses or write to
+* the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+* MA 02110-1301 USA.
+* 
+* You can contact Funambol, Inc. headquarters at 643 Bair Island Road, Suite 
+* 305, Redwood City, CA 94063, USA, or at email address info@funambol.com.
+* 
+* The interactive user interfaces in modified source and object code versions
+* of this program must display Appropriate Legal Notices, as required under
+* Section 5 of the GNU Affero General Public License version 3.
+* 
+* In accordance with Section 7(b) of the GNU Affero General Public License
+* version 3, these Appropriate Legal Notices must retain the display of the
+* "Powered by Funambol" logo. If the display of the logo is not reasonably 
+* feasible for technical reasons, the Appropriate Legal Notices must display
+* the words "Powered by Funambol".
+*/
 
 # include <cppunit/extensions/TestFactoryRegistry.h>
 # include <cppunit/extensions/HelperMacros.h>
@@ -44,6 +44,7 @@ USE_NAMESPACE
 
 class ArrayListTest : public CppUnit::TestFixture {
     CPPUNIT_TEST_SUITE(ArrayListTest);
+    CPPUNIT_TEST(testAddGet);
     CPPUNIT_TEST(removeFirst);
     CPPUNIT_TEST(removeLast);
     CPPUNIT_TEST(removeMiddle);
@@ -52,6 +53,7 @@ class ArrayListTest : public CppUnit::TestFixture {
     CPPUNIT_TEST(iterateAndDelete);
     CPPUNIT_TEST(iterateAndDelete2);
     CPPUNIT_TEST(iterateAndAddDelete);
+    CPPUNIT_TEST(testManyItems);
     CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -68,10 +70,11 @@ public:
 
         ac.add(a);
         ac.add(c);
-        
+
         ab.add(a);
         ab.add(b);
     }
+
     void tearDown() {
         abc.clear();
         bc.clear();
@@ -80,6 +83,17 @@ public:
     }
 
 private:
+
+    void testAddGet() {
+        ArrayList al;
+        StringBuffer test("test");
+
+        al.add(test);
+        CPPUNIT_ASSERT(*(StringBuffer *)al.get(0) == test);
+        CPPUNIT_ASSERT(*(StringBuffer *)al[0] == test);
+        CPPUNIT_ASSERT(al[1] == NULL);
+    }
+
     void removeFirst() {
         ArrayList l = abc;
 
@@ -122,7 +136,7 @@ private:
         l.next();
         CPPUNIT_ASSERT_EQUAL(true, l.last());       
     }
-    
+
     bool equal(ArrayList &first, ArrayList &second) {
         ArrayElement *first_e = first.front();
         int index = 0;
@@ -135,8 +149,8 @@ private:
 
             if (first_e && !second_e ||
                 !first_e && second_e) {
-                // different length
-                return false;
+                    // different length
+                    return false;
             }
 
             if (*(StringBuffer *)first_e != *(StringBuffer *)second_e) {
@@ -246,6 +260,45 @@ private:
         CPPUNIT_ASSERT(item && *((StringBuffer *)item) == b);
     }
 
+    void testManyItems() {
+        ArrayList many;
+
+        const int max = 10000;
+        int i;
+        StringBuffer s;
+        clock_t start = clock();
+
+        // insert many items
+        for (i=0; i<max; i++) {
+            s.sprintf("item%d", i);
+            many.add(s);
+        }
+        printf("\n\tInsert: %fs", (float)(clock()-start)/CLOCKS_PER_SEC);
+
+        // access them using index
+        start=clock();
+        for (i=0; i<max; i++) {
+            s.sprintf("item%d", i);
+            CPPUNIT_ASSERT(*(StringBuffer *)many[i] == s);
+        }
+        printf("\n\tAccess index: %fs", (float)(clock()-start)/CLOCKS_PER_SEC);
+
+        // access them using iterator methods
+        StringBuffer *p;
+        start=clock();
+        for (i=0, p=(StringBuffer*)many.front(); p; p=(StringBuffer*)many.next(), i++) {
+            s.sprintf("item%d", i);
+            CPPUNIT_ASSERT(*p == s);
+        }
+        printf("\n\tAccess iterator methods: %fs", (float)(clock()-start)/CLOCKS_PER_SEC);
+
+        // remove them 
+        start=clock();
+        for (i=1; i<max; i++) {
+            many.removeElementAt(i);
+        }
+        printf("\n\tRemove: %fs", (float)(clock()-start)/CLOCKS_PER_SEC);
+    }
 
     ArrayList abc, bc, ac, ab, empty;
 };
