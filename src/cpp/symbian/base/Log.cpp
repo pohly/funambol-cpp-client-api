bcf770c25e1ca32d790ec63f7f31a60c17f0fe57
db <db@3966be30-755a-0410-815e-dd79f492ac3c>
2009-06-08 15:21:25 +0000

    check for existing log semaphore (CreateGlobal is called only if OpenGlobal fails) - merged from branch v7.1
    git-svn-id: https://client-sdk.forge.funambol.org/svn/client-sdk/branches/cpp-sdk/v8@872 3966be30-755a-0410-815e-dd79f492ac3c

--- src/cpp/symbian/base/Log.cpp
+++ src/cpp/symbian/base/Log.cpp
@@ -75,15 +75,20 @@ SymbianLog::SymbianLog(bool resetLog, const char* path, const char* name)
     StringBuffer nameSb(n);
     iLogFileName.Assign(charToNewBuf(nameSb.c_str()));
     
-    
-    // Create a semaphore, to avoid accessing the FileSystem at
-    // the same time by different threads.
-    // The semaphore is global, so that it could be used also by
-    // other processes that (in future) will use this Log.
-    err = iSemaphore.CreateGlobal(KLogSemaphoreName, 1);
-    if (err != KErrNone) {
-        setError(ERR_SEMAPHORE_CREATION, ERR_SEMAPHORE_CREATION_MSG);
-    }
+    // try open log 
+    err = iSemaphore.OpenGlobal(KLogSemaphoreName);  
+
+    if (err == KErrNotFound) {
+        // Create a semaphore, to avoid accessing the FileSystem at
+        // the same time by different threads.
+        // The semaphore is global, so that it could be used also by
+        // other processes that (in future) will use this Log.
+        err = iSemaphore.CreateGlobal(KLogSemaphoreName, 1);
+        if (err != KErrNone) {
+            setError(ERR_SEMAPHORE_CREATION, ERR_SEMAPHORE_CREATION_MSG);
+        }
+    } 
+
     iSemaphore.Wait();
 
     
